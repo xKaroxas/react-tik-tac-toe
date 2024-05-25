c0448d6f5c6fdfde252182a38523b1a730686da4
karoxas <karoxas20@gmail.com>
2024-05-25 14:35:42 -0300

    First Commit

---
+++ src/components/WinnerModal.jsx
@@ -0,0 +1,20 @@
+import { Square } from "./Square.jsx";
+
+export function WinnerModal({ winner, resetGame }) {
+ if (winner === null) return null;
+
+ const winnerText = winner === false ? "Empate" : "Ganó:";
+
+ return (
+  <section className="winner">
+   <div className="text">
+    <h1>{winnerText}</h1>
+    <h2>{winner === false ? "Empate" : "Gano:"}</h2>
+    <header className="win">{winner && <Square>{winner}</Square>}</header>
+    <footer>
+     <button onClick={resetGame}>Empezar de nuevo</button>
+    </footer>
+   </div>
+  </section>
+ );
+}
